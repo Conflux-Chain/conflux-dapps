efca91af7191789f0e74a4accb9f5f768c1a7336
angelia <greenfish84@126.com>
2022-03-18 17:38:08 +0800

    fix: i18n

--- dapps/shuttle-flow/src/i18n.js
+++ dapps/shuttle-flow/src/i18n.js
@@ -2,15 +2,20 @@ import i18next from 'i18next'
 import {initReactI18next} from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import en from './assets/locales/en.json'
+import zh from './assets/locales/zh-CN.json'
+
+const resources = {
+  en: {translation: en},
+  zh: {translation: zh},
+}
 
 if (window.__POWERED_BY_QIANKUN__ ) {
   i18next
   .use(Backend)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: `./locales/{{lng}}.json`,
-    },
+    resources,
     react: {
       useSuspense: true,
     },
@@ -27,9 +32,7 @@ if (window.__POWERED_BY_QIANKUN__ ) {
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: `./locales/{{lng}}.json`,
-    },
+    resources,
     react: {
       useSuspense: true,
     },
@@ -40,6 +43,5 @@ if (window.__POWERED_BY_QIANKUN__ ) {
     interpolation: {escapeValue: false},
   })
 }
-
 
 export default i18next
