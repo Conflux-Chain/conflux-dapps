723eff3458f24ad5a2f2ea8fa9449d62e89dc3ae
SSSensational <weiwen.liu@confluxnetwork.org>
2022-03-21 19:01:42 +0800

    fix: sf entry

--- dapps/dapp-box/src/vite-env.d.ts
+++ dapps/dapp-box/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_SHUTTLE_FLOW_URL: string
-    // 更多环境变量...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
