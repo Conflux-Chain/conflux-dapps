94061c2417f6212e0eee47c0556fe0ebc0143305
SSSensational <weiwen.liu@confluxnetwork.org>
2022-03-23 13:47:06 +0800

    rename

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     content: [
         path.resolve(__dirname, '../../dapps/cross-space/src/**/*.{js,ts,jsx,tsx}'),
-        path.resolve(__dirname, '../../dapps/conflux-hub/src/**/*.{js,ts,jsx,tsx}'),
+        path.resolve(__dirname, '../../dapps/dapp-box/src/**/*.{js,ts,jsx,tsx}'),
         path.resolve(__dirname, '../../packages/common/components/**/*.{js,ts,jsx,tsx}'),
         path.resolve(__dirname, '../../packages/common/modules/**/*.{js,ts,jsx,tsx}'),
     ],
