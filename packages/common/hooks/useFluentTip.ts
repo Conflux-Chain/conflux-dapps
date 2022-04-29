02cf9652fe3760d4eeda36a6db1e7c1f2a5ce8ad
SSSensational <sss18201672034@163.com>
2022-04-29 12:40:31 +0800

    fix: fluent Notification bug

--- packages/common/hooks/useFluentTip.ts
+++ packages/common/hooks/useFluentTip.ts
@@ -1,22 +1,19 @@
-import { useEffect, useRef } from 'react';
-import { useChainId as useFluentChainId } from '@cfxjs/use-wallet';
-import { useChainId as useMetaMaskChainId } from '@cfxjs/use-wallet/dist/ethereum';
+import { useEffect } from 'react';
+import { provider as metaMaskProvider } from '@cfxjs/use-wallet/dist/ethereum';
 import { showToast } from 'common/components/tools/Toast';
 
 const useFluentTip = (dappName: string) => {
-    const toastRef = useRef<string | number | null>(null);
-    const fluentChainId = useFluentChainId();
-    const metaMaskChainId = useMetaMaskChainId();
-
     useEffect(() => {
-        if (toastRef.current) return;
-        if (fluentChainId === '9007199254740991' || metaMaskChainId === '9007199254740991') {
-            toastRef.current = showToast(
-                `${dappName} does not support connecting two chains via Fluent simultaneously, recently recommend connecting Conflux eSpace and other EVM chains via MetaMask.`,
-                { key: 'fluent-tip', type: 'failed', showClose: false, duration: 0, special: true }
-            );
-        }
-    }, [fluentChainId, metaMaskChainId]);
+        if (!metaMaskProvider) return;
+        metaMaskProvider.request({ method: `cfx_chainId` })
+            .then(() => {
+                showToast(
+                    `${dappName} does not support connecting two chains via Fluent simultaneously, recently recommend connecting Conflux eSpace and other EVM chains via MetaMask.`,
+                    { key: 'fluent-tip', type: 'failed', showClose: false, duration: 0, special: true }
+                );
+            })
+            .catch(() => {});
+    }, []);
 };
 
 export default useFluentTip;
