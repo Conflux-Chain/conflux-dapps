3a297dad8443241b04eb8e93bef82b1610e106bc
tangxuezhi <0x74616e67@gmail.com>
2022-11-01 11:01:51 +0800

    payment-feat: add env grouping

--- dapps/payment/src/utils/constants.ts
+++ dapps/payment/src/utils/constants.ts
@@ -3,6 +3,14 @@ import Networks from 'common/conf/Networks';
 export const RPC: string = Networks.eSpace.rpcUrls[0];
 export const CHAIN_ID: string = Networks.eSpace.chainId;
 
+export const ENV = location.host.startsWith('confluxhub')
+    ? 'prod'
+    : location.host.startsWith('stage')
+    ? 'stage'
+    : location.host.startsWith('test')
+    ? 'test'
+    : 'dev'; // dev-internal or localhost
+
 export enum OP_ACTION {
     'add',
     'edit',
@@ -23,3 +31,28 @@ export const CONTRACT_ERRORS = {
     exceedDuration: 'The duration cannot exceed 10 years.',
     expirationExceed: 'Expiration time exceeded.',
 };
+
+export const showWeb3PaywallEntry = ENV === 'dev' || ENV === 'test';
+
+export const CONTRACT_ADDRESSES = {
+    prod: {
+        multicall: '',
+        appRegistry: '',
+        util: '', // read functions contract
+    },
+    stage: {
+        multicall: '',
+        appRegistry: '',
+        util: '',
+    },
+    test: {
+        multicall: '0xd59149a01f910c3c448e41718134baeae55fa784',
+        appRegistry: '0x1B31A3AB02F74b7c340B6d2772a7e0B1A3108721',
+        util: '0x27D2DF0f5cc75C690647f9A05EF989F57c10614B',
+    },
+    dev: {
+        multicall: '0xd59149a01f910c3c448e41718134baeae55fa784',
+        appRegistry: '0xD24B98Ed55B46d69f8f1918E1EeAFd7DB5E6Ee4E',
+        util: '0xB4611cfB227379702FC2565f156051C8913292d2',
+    },
+}[ENV];
