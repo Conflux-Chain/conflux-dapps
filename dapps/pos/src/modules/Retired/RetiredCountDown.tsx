2798442773b66f0fab1df6c558efbbe9c149f61b
SSSensational <37439786+SSSensational@users.noreply.github.com>
2023-01-30 17:56:39 +0800

    fix: pos router && countdown

--- dapps/pos/src/modules/Retired/RetiredCountDown.tsx
+++ dapps/pos/src/modules/Retired/RetiredCountDown.tsx
@@ -12,13 +12,18 @@ const RetiredCountDown: React.FC = () => {
     useLayoutEffect(() => {
         if (!lastestRetireHeight) return;
         const startTimerNotifier = (posCurrentHeight: number) => {
-            const endTime = Date.now() + (lastestRetireHeight - posCurrentHeight) * 60 * 1000;
-            const unitsDOM = Object.fromEntries(units.map(unit => [unit, Array.from(domRef.current?.querySelector(`.${unit}`)?.querySelectorAll('span') as unknown as Array<HTMLSpanElement>)]));
+            const endTime = (lastestRetireHeight - posCurrentHeight) * 60 * 1000;
+            const unitsDOM = Object.fromEntries(
+                units.map((unit) => [
+                    unit,
+                    Array.from(domRef.current?.querySelector(`.${unit}`)?.querySelectorAll('span') as unknown as Array<HTMLSpanElement>),
+                ])
+            );
             const timerUnit: Parameters<typeof timerNotifier.addUnit>[0] = {
                 key: 'pos-retired-countdown',
                 type: 'second',
                 update: (remainTime) => {
-                    units.forEach(unit => {
+                    units.forEach((unit) => {
                         if (!unitsDOM[unit]) return;
                         for (let i = 0, len = unitsDOM[unit].length; i < len; i++) {
                             unitsDOM[unit][i].innerText = remainTime[unit].charAt(i);
@@ -26,48 +31,53 @@ const RetiredCountDown: React.FC = () => {
                     });
                 },
                 timing: endTime,
-            }
+            };
             timerNotifier.addUnit(timerUnit);
-        }
+        };
 
         const posCurrentHeight = posStore.getState().posCurrentHeight;
         let unsubPosCurrentHeight: VoidFunction | null = null;
         if (posCurrentHeight) {
             startTimerNotifier(posCurrentHeight);
         } else {
-            unsubPosCurrentHeight = posStore.subscribe(state => state.posCurrentHeight, (posCurrentHeight) => {
-                if (!posCurrentHeight) return;
-                unsubPosCurrentHeight?.();
-                startTimerNotifier(posCurrentHeight);
-            });
+            unsubPosCurrentHeight = posStore.subscribe(
+                (state) => state.posCurrentHeight,
+                (posCurrentHeight) => {
+                    if (!posCurrentHeight) return;
+                    unsubPosCurrentHeight?.();
+                    startTimerNotifier(posCurrentHeight);
+                }
+            );
         }
 
         return () => {
             unsubPosCurrentHeight?.();
             timerNotifier.deleteUnit('pos-retired-countdown');
-        }
+        };
     }, [lastestRetireHeight]);
 
     return (
         <div className="flex justify-center gap-[16px]" ref={domRef}>
-            {units.map(unit => <CountdownUnit key={unit} unit={unit} />)}
+            {units.map((unit) => (
+                <CountdownUnit key={unit} unit={unit} />
+            ))}
         </div>
     );
-}
+};
 
-const CountdownUnit: React.FC<{ unit: typeof units[number]; }> = ({ unit }) => {
+const CountdownUnit: React.FC<{ unit: (typeof units)[number] }> = ({ unit }) => {
     return (
-        <div className='w-[72px] h-[72px] rounded-[8px] overflow-hidden'>
-            <div className={`flex justify-center items-center gap-[1px] posRetired-countdown-unit relative h-[52px] leading-[52px] text-[32px] text-white font-bold bg-[#808BE7] governance-shadow ${unit}`}>
-                <span className='text-right'></span>
-                <span className='text-left'></span>
+        <div className="w-[72px] h-[72px] rounded-[8px] overflow-hidden">
+            <div
+                className={`flex justify-center items-center gap-[1px] posRetired-countdown-unit relative h-[52px] leading-[52px] text-[32px] text-white font-bold bg-[#808BE7] governance-shadow ${unit}`}
+            >
+                <span className="text-right"></span>
+                <span className="text-left"></span>
             </div>
 
-            <div className='h-[20px] leading-[20px] text-[12px] text-[#808BE7] text-center bg-[#F8F9FE]'>
-                {unit}
-            </div>
+            <div className="h-[20px] leading-[20px] text-[12px] text-[#808BE7] text-center bg-[#F8F9FE]">{unit}</div>
         </div>
     );
-}
+};
 
 export default RetiredCountDown;
--- dapps/pos/src/modules/index.tsx
+++ dapps/pos/src/modules/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
 import { useAccount as useConfluxAccount } from '@cfxjs/use-wallet-react/conflux/Fluent';
 import { startTrack, usePosAccount, usePosAccountInfo } from 'pos/src/store';
 import Spin from 'common/components/Spin';
@@ -11,15 +11,15 @@ import Retired from './Retired';
 
 export const isAcceptedPosGuide = () => localStorage.getItem('posAcceptedGuide') !== null;
 
-const Apps: React.FC = () => {
-    useEffect(startTrack, []);
 
+const useAuth = (pathname?: string) => {
     const navigate = useNavigate();
     const posAccount = usePosAccount();
     const posAccountInfo = usePosAccountInfo();
     const confluxAccount = useConfluxAccount();
 
     useEffect(() => {
+        if (pathname === '/pos/auth' || pathname === '/pos/auth/') return;
         if (!isAcceptedPosGuide()) {
             navigate('/pos/guide');
             return;
@@ -37,22 +37,32 @@ const Apps: React.FC = () => {
         } else if (posAccount === null) {
             navigate('/pos/register');
         }
-    }, [confluxAccount, posAccount, posAccountInfo]);
+    }, [confluxAccount, posAccount, posAccountInfo, pathname]);
+}
+
+const Auth: React.FC = () => {
+    useAuth();
+    return (
+        <Delay delay={500}>
+            <Spin className="text-[48px] mt-[120px] mx-auto" />
+        </Delay>
+    );
+};
+
+const Apps: React.FC = () => {
+    useEffect(startTrack, []);
+    
+    const location = useLocation();
+    useAuth(location.pathname);
 
     return (
         <Routes>
-            <Route
-                index
-                element={
-                    <Delay delay={500}>
-                        <Spin className="text-[48px] mt-[120px] mx-auto" />
-                    </Delay>
-                }
-            />
+            <Route key="auth" path="auth" element={<Auth />} />
             <Route key="pos-guide" path="guide" element={<Guide isRegister={false} />} />
             <Route key="pos-register" path="register" element={<Register />} />
             <Route key="pos-increase" path="increase" element={<Increase />} />
             <Route key="pos-retired" path="retired" element={<Retired />} />
+            <Route path="*" element={<Navigate to="auth" />} />
         </Routes>
     );
 };
