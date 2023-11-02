f1ee51575cf0d5b12acb660cd0209a7a77bbc81b
shine <shouhui.sun@gmail.com>
2023-11-02 16:58:41 +0800

    feat: Restore governanceV2

--- dapps/governance/src/modules/Vote/Proposals/handleVote.ts
+++ dapps/governance/src/modules/Vote/Proposals/handleVote.ts
@@ -3,9 +3,11 @@ import { showWaitWallet, showActionSubmitted, hideWaitWallet } from 'common/comp
 import { showToast } from 'common/components/showPopup/Toast';
 import { governanceContract, governanceContractAddress } from "governance/src/store/contracts";
 import Networks from "common/conf/Networks";
+import { convertCfxToHex } from 'common/utils/addressUtils';
 
+export interface ProposalType { poolAddress: string | undefined, proposalId: number; optionId: number; power: string }
 
-const handleVote = async ({ proposalId, optionId }: { proposalId: number; optionId: number; }) => {
+const handleVote = async ({ poolAddress, proposalId, optionId, power }: ProposalType) => {
     if (typeof proposalId !== 'number' || typeof optionId !== 'number') return;
     let waitFluentKey: string | number = null!;
     let transactionSubmittedKey: string | number = null!;
@@ -14,8 +16,9 @@ const handleVote = async ({ proposalId, optionId }: { proposalId: number; option
         waitFluentKey = showWaitWallet('Fluent', { key: 'Vote' });
         const TxnHash = await sendTransaction({
             to: governanceContractAddress,
-            data: governanceContract.vote(proposalId, optionId).encodeABI(),
-        });
+            data: poolAddress ? governanceContract.voteThroughPosPool(convertCfxToHex(poolAddress), proposalId, optionId, power).encodeABI()
+                : governanceContract.vote(proposalId, optionId, power).encodeABI(),
+        })
         transactionSubmittedKey = showActionSubmitted(TxnHash, 'Vote', { duration: 6666, blockExplorerUrl: Networks.core.blockExplorerUrls[0] });
         return true;
     } catch (err) {
