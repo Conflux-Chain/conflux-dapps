6e872c3168b7e2201acfa9f1f7b303875e9a9c3a
SSSensational <37439786+SSSensational@users.noreply.github.com>
2024-02-21 17:29:28 +0800

    fix: isSameChainNativeWallet

--- packages/common/hooks/useIsSameChainNativeWallet.ts
+++ packages/common/hooks/useIsSameChainNativeWallet.ts
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
-import { store as confluxStore} from '@cfxjs/use-wallet-react/conflux/Fluent';
-import { store as ethereumStore, useStatus as useStatueEthereum } from '@cfxjs/use-wallet-react/ethereum';
+import { useChainId as useConfluxChainId } from '@cfxjs/use-wallet-react/conflux/Fluent';
+import { useChainId as useEthereumChainId, useStatus as useEthereumStatus } from '@cfxjs/use-wallet-react/ethereum';
 import { useChainIdNative } from 'governance/src/store/lockDays&blockNumber';
 import { spaceSeat } from 'common/conf/Networks';
 
 export const isSameChainNativeWallet = () => {
     const [isSame, setIsSame] = useState(false);
     const chainIdNative = useChainIdNative();
-    const confluxChainId = confluxStore.getState().chainId;
-    const ethereumChainId = ethereumStore.getState().chainId;
-    const isEthereumActive = useStatueEthereum() === 'active';
+    const confluxChainId = useConfluxChainId();
+    const ethereumChainId = useEthereumChainId();
+    const ethereumStatus = useEthereumStatus();
+    const isEthereumActive = ethereumStatus === 'active';
 
     useEffect(() => {
         const chainId = chainIdNative === confluxChainId ? confluxChainId : chainIdNative === ethereumChainId ? ethereumChainId : '';
@@ -20,7 +21,7 @@ export const isSameChainNativeWallet = () => {
         }
 
         setIsSame(chainIdNative === chainId);
-    },[chainIdNative, confluxChainId, ethereumChainId])
+    }, [chainIdNative, confluxChainId, ethereumChainId, isEthereumActive]);
 
     return isSame;
-}
+};
